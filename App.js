371a923bc2e47367387d0999d7081b9c6662a97d
Ester Costa de Souza <alwesterc@gmail.com>
2022-04-29 16:23:23 -0300

    termino do curso de react native da alura

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { StatusBar, SafeAreaView, View } from 'react-native';
-import Cesta from './src/telas/Cesta'
+import { StatusBar, SafeAreaView } from 'react-native';
 import { 
   useFonts, 
   Montserrat_400Regular, 
   Montserrat_700Bold 
 } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
+
+import Cesta from './src/telas/Cesta'
+import mock from './src/mocks/cesta'
 
 export default function App() {
   const [fonteCarregada] = useFonts({
     'MontserratRegular': Montserrat_400Regular,
-    'MonteserratBold': Montserrat_700Bold
+    'MontserratBold': Montserrat_700Bold
   });
 
   if(!fonteCarregada) {
-    return <View/>
+    return <AppLoading/>
   }
 
   return (
-    <SafeAreaView>
-      <Cesta/>
+    <SafeAreaView style={{flex:1}}>
+      <Cesta { ...mock }/>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
